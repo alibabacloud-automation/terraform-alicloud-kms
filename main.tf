ee43e10605cb49ad863aee5ed1d2c16993acf433
liuyuan <709503583@qq.com>
2021-12-30 19:07:26 +0800

    Improves the module examples/complete

--- main.tf
+++ main.tf
@@ -1,27 +1,26 @@
 locals {
-  this_kms_key_id    = var.use_existing_key == true || var.existing_key_id != "" ? var.existing_key_id : concat(alicloud_kms_key.kms.*.id, [""])[0]
-  ciphertext_blob    = var.decrypt == true || alicloud_kms_ciphertext.kms.0.ciphertext_blob != "" ? alicloud_kms_ciphertext.kms.0.ciphertext_blob : null
-  encryption_context = var.decrypt == true || alicloud_kms_ciphertext.kms.0.encryption_context != "" ? alicloud_kms_ciphertext.kms.0.encryption_context : null
-
+  this_kms_key_id    = var.use_existing_key || var.existing_key_id != "" ? var.existing_key_id : concat(alicloud_kms_key.kms.*.id, [""])[0]
+  ciphertext_blob    = var.decrypt ? var.ciphertext_blob : null
+  encryption_context = var.decrypt ? var.encryption_context : null
 }
 
 resource "alicloud_kms_key" "kms" {
-  count                   = var.use_existing_key == true ? 0 : var.create_kms ? 1 : 0
-  description             = var.description
-  key_usage               = var.key_usage
-  deletion_window_in_days = var.deletion_window_in_days
-  is_enabled              = var.is_enabled
+  count                  = var.use_existing_key ? 0 : var.create_kms ? 1 : 0
+  description            = var.description
+  key_usage              = var.key_usage
+  pending_window_in_days = var.pending_window_in_days != "" ? var.pending_window_in_days : var.deletion_window_in_days
+  status                 = var.status != "" ? var.status : var.is_enabled ? "Enabled" : "Disabled"
 }
 
 resource "alicloud_kms_ciphertext" "kms" {
-  count              = var.existing_key_id != "" ? var.existing_key_id : var.encrypt ? 1 : 0
+  count              = var.encrypt ? 1 : 0
   plaintext          = var.plaintext
-  key_id             = concat(alicloud_kms_key.kms.*.id, [""])[0]
+  key_id             = local.this_kms_key_id
   encryption_context = var.encryption_context
 }
 
 data "alicloud_kms_plaintext" "kms" {
+  count              = var.decrypt ? 1 : 0
   ciphertext_blob    = local.ciphertext_blob
   encryption_context = local.encryption_context
 }
-
--- outputs.tf
+++ outputs.tf
@@ -1,46 +1,47 @@
 # kms key
 output "this_kms_key_id" {
   description = "The ID of the key."
-  value       = alicloud_kms_key.kms.*.id
+  value       = concat(alicloud_kms_key.kms.*.id, [""])[0]
 }
 
 output "this_kms_key_arn" {
   description = "The Alicloud Resource Name (ARN) of the key."
-  value       = alicloud_kms_key.kms.*.arn
+  value       = concat(alicloud_kms_key.kms.*.arn, [""])[0]
 }
 
 output "this_kms_key_description" {
   description = "The description of the key."
-  value       = alicloud_kms_key.kms.*.description
+  value       = concat(alicloud_kms_key.kms.*.description, [""])[0]
 }
 
 output "this_kms_key_usage" {
   description = "Specifies the usage of CMK."
-  value       = alicloud_kms_key.kms.*.key_usage
+  value       = concat(alicloud_kms_key.kms.*.key_usage, [""])[0]
 }
 
 output "this_kms_key_deletion_window_in_days" {
   description = "During pre-deletion days."
-  value       = alicloud_kms_key.kms.*.deletion_window_in_days
+  value       = concat(alicloud_kms_key.kms.*.deletion_window_in_days, [""])[0]
 }
 
 output "this_kms_key_is_enabled" {
   description = "Whether the key is enabled."
-  value       = alicloud_kms_key.kms.*.is_enabled
+  value       = concat(alicloud_kms_key.kms.*.is_enabled, [""])[0]
 }
 
+#encrypt
 output "this_kms_ciphertext_ciphertext_blob" {
   description = "The ciphertext of the data key encrypted with the primary CMK version."
-  value       = alicloud_kms_ciphertext.kms.*.ciphertext_blob
+  value       = concat(alicloud_kms_ciphertext.kms.*.ciphertext_blob, [""])[0]
 }
 
 #decrypt
 output "this_decrypt_plaintext" {
   description = "The decrypted plaintext."
-  value       = data.alicloud_kms_plaintext.kms.plaintext
+  value       = concat(data.alicloud_kms_plaintext.kms.*.plaintext, [""])[0]
 }
 
 output "this_decrypt_key_id" {
   description = "The globally unique ID of the CMK. It is the ID of the CMK used to decrypt ciphertext."
-  value       = data.alicloud_kms_plaintext.kms.key_id
+  value       = concat(data.alicloud_kms_plaintext.kms.*.key_id, [""])[0]
 }
